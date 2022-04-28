16b1a52de4b9d234414416bdb89ea94b22048c09
Geovannysan <geovanny1santintiago@gmail.com>
2022-04-28 18:07:51 -0500

    acs

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var cors = require('cors')
 const dep = require('./Router/Peliculasrouter')
 const { createpeliculas, verpelicoual, createsala } = require("./libs/initialSetup");
 const { Asientos } = require("./libs/Asientos");
-const socket = require('socket.io'), http = require('http');
 const app = express()
-require('dotenv').config()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+require('dotenv').config()
+var messages=[];
 
 
 //setiammos puerto
@@ -19,21 +21,26 @@ app.use(express.json())
 //midelware cors 
 app.use(cors())
 
+io.on("connection", function (socket) {
+  console.log("Alguien se ha conectado "+socket);
+
+});
 //conexcion mongodb
-const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.ehrf8.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+/*const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.ehrf8.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose.connect(uri, {
     useNewUrlParser: true, useUnifiedTopology: true,
     useFindAndModify: false
 }).then(() => console.log('conectado a mongodb'))
     .catch(e => console.log('error de conexión', e))
-
+*/
 
 app.use('/', dep)
-createpeliculas();
-createsala();
+//createpeliculas();
+//createsala();
 //console.log(Asientos(10, 6))//
 //verpelicoual();
 //puerto escuchando
+
 app.listen(app.get('port'), () => {
     console.log("servidor escuchando ", app.get('port'))
 })
